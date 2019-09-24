d630f8b226927d4f8bbcb598d17c05f4b67712e6
Shashank Shekhar <shashank7200@gmail.com>
2019-09-24 15:14:08 +0530

    updated routes for the application

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import  Auth  from './components/Auth';
+import React from "react";
+import firebase from "firebase";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import { Auth, TwitterConnect } from "./components";
 
-const App = () => {
-  console.log(process.env.REACT_APP_NOT_SECRET_CODE)
-  return (
-    <div className="App">
-      <Auth />  
-    </div>
+const PrivateRoute = props => {
+  return firebase.auth().currentUser != null ||
+    localStorage.getItem("twitterHelpdesk.expectSignIn") ? (
+    <Route {...props} />
+  ) :
+  (
+    <Redirect
+      to={{
+        pathname: "/"
+      }}
+    />
   );
-}
+};
 
-export default App;
+const Routes = () => {
+  return (
+    <Router>
+      <Route exact path="/" component={Auth} />
+      <PrivateRoute path="/signedIn" component={TwitterConnect} />
+    </Router>
+  );
+};
+
+export default Routes;
