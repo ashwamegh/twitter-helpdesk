58bab9d8ddb97ca373cf67531947be7091dc9d2b
Shashank Shekhar <shashank7200@gmail.com>
2019-09-25 00:14:57 +0530

    created user timeline route to fetch latest tweets

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,24 +3,33 @@ const router = express.Router();
 const Twitter = require('twitter');
 require('dotenv').config();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
-router.post('/twitter', (req, res) => {
-  const { key, secret } = req.body;
-  const client = new Twitter({
+
+const createTwitterClient = (key, secret) => {
+  return new Twitter({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
     access_token_key: key,
     access_token_secret: secret,
   }); 
+}
 
-  client.get('favorites/list', function(error, tweets, response) {
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/twitter/tweets', (req, res) => {
+  const { key, secret } = req.body;
+  const client = createTwitterClient(key, secret);
+  
+  const params = {
+    exclude_replies: false
+  }
+
+  client.get('statuses/user_timeline',params, (error, tweets, response) => {
     if(error) console.log(error);
-    console.log(tweets);  // The favorites.
-    console.log(response);  // Raw response object.
+    res.json(tweets);  // The favorites.
   });
 
 })
