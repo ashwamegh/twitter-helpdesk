fcb53daf5efe2f23eb635aa0ece22b8d17b45865
Shashank Shekhar <shashank7200@gmail.com>
2019-09-26 01:36:45 +0530

    added and modified express routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,13 +4,15 @@ const Twitter = require('twitter');
 require('dotenv').config();
 const Pusher = require('pusher');
 
-const channelsClient = new Pusher({
-  appId: process.env.PUSHER_APP_ID,
-  key: process.env.PUSHER_APP_KEY,
-  secret: process.env.PUSHER_APP_SECRET,
-  cluster: 'ap2',
-  encrypted: true
-});
+const createPusherChannelClient = () => {
+  return new Pusher({
+    appId: process.env.PUSHER_APP_ID,
+    key: process.env.PUSHER_APP_KEY,
+    secret: process.env.PUSHER_APP_SECRET,
+    cluster: 'ap2',
+    encrypted: true
+  });
+}
 
 const createTwitterClient = (key, secret) => {
   return new Twitter({
@@ -30,12 +32,28 @@ router.post('/twitter/tweets', (req, res) => {
   const { key, secret } = req.body;
   const client = createTwitterClient(key, secret);
   const params = {
-    exclude_replies: false
+    exclude_replies: false,
+    count: 50
   }
     client.get('statuses/user_timeline',params, (error, tweets, response) => {
       if(error) console.log(error);
-      channelsClient.trigger('twitter-helpdesk', 'my-tweets', tweets);
       res.json(tweets);
+    })
+  })
+
+  router.post('/twitter/reply',  (req, res) => {
+    const { key, secret, statusID, status } = req.body;
+    const client = createTwitterClient(key, secret);
+    const channelsClient = createPusherChannelClient();
+    const params = {
+      in_reply_to_status_id: statusID,
+      status
+    }
+
+    client.post('/statuses/update', params , (error, tweet, response) => {
+      channelsClient.trigger('chat', 'message', tweet);
+      if(error) res.sendStatus(500);
+      res.json(tweet);
     })
   })
 
