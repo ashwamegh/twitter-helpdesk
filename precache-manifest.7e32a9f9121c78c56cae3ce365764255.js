e4cbd584dedd225b5ef9019f40f5740f5dd228b1
Shashank Shekhar <shashank7200@gmail.com>
2019-09-26 02:17:38 +0530

    Updates

---
+++ precache-manifest.7e32a9f9121c78c56cae3ce365764255.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d59474c1896015d3d713255de0ad78d9",
+    "url": "/twitter-helpdesk/index.html"
+  },
+  {
+    "revision": "ff65d75d7775d68f0bcf",
+    "url": "/twitter-helpdesk/static/css/main.14b92a9d.chunk.css"
+  },
+  {
+    "revision": "e9f13f8ac2395db106af",
+    "url": "/twitter-helpdesk/static/js/2.a332955a.chunk.js"
+  },
+  {
+    "revision": "ff65d75d7775d68f0bcf",
+    "url": "/twitter-helpdesk/static/js/main.77858123.chunk.js"
+  },
+  {
+    "revision": "0a1cf89bffbc3eb2618c",
+    "url": "/twitter-helpdesk/static/js/runtime-main.d69e2295.js"
+  },
+  {
+    "revision": "902f2b3a5fa5b6cc5b4b80ab36135404",
+    "url": "/twitter-helpdesk/static/media/twitter-icon.902f2b3a.png"
+  }
+]);
